3ddb6b22e1351555f59e51d90567bed46fc87d8f
hugosmoreira <hugo.n4@gmail.com>
2021-08-11 23:35:29 -0700

    widgets dropdown

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import Dropdown from "./components/Dropdown";
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end JS FrameWork",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite among programers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
+
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "Shade of Blue",
+    value: "blue",
+  },
+];
+
+export default () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Dropdown
+        options={options}
+        onSelectedChange={setSelected}
+        selected={selected}
+      />
     </div>
   );
-}
-
-export default App;
+};
